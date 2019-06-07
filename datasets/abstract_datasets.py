791effa1c087e7eac372d3105551773dfc7d4f57
Madan <madantrg@umich.edu>
2019-06-07 17:04:21 -0400

    Minor edits to abstract data class method to getclips function for recognition, need to edit it to handle json. including hmdb51 dataset

--- datasets/abstract_datasets.py
+++ datasets/abstract_datasets.py
@@ -34,6 +34,51 @@ class RecognitionDataset(VideoDataset):
         super(RecognitionDataset, self).__init__(*args, **kwargs)
 
     def getClips(self):
+        data_path  = os.path.join('/z/dat', self.dataset_dir)
+        label_dict = {'brush_hair': 0,      'cartwheel': 1,
+                      'catch': 2,           'chew': 3, 
+                      'clap': 4,            'climb_stairs': 5, 
+                      'climb': 6,           'dive': 7,
+                      'draw_sword': 8,      'dribble': 9,
+                      'drink': 10,          'eat': 11,
+                      'fall_floor': 12,     'fencing': 13,
+                      'flic_flac': 14,      'golf': 15,
+                      'handstand': 16,      'hit': 17,
+                      'hug': 18,            'jump': 19,
+                      'kick_ball': 20,      'kick': 21,
+                      'kiss': 22,           'laugh': 23,
+                      'pick': 24,           'pour': 25,
+                      'pullup': 26,         'punch': 27,
+                      'push': 28,           'pushup': 29,
+                      'ride_bike': 30,      'ride_horse': 31,
+                      'run': 32,            'shake_hands': 33,
+                      'shoot_ball': 34,     'shoot_bow': 35,
+                      'shoot_gun': 36,      'sit': 37, 
+                      'situp': 38,          'smile': 39,
+                      'smoke': 40,          'somersault': 41,
+                      'stand': 42,          'swing_baseball': 43,
+                      'sword_exercise': 44, 'sword': 45,
+                      'talk': 46,           'throw': 47,
+                      'turn': 48,           'walk': 49,
+                      'wave': 50}
+        
+   
+        base_path = os.path.join(data_path, self.train_or_val+'images')
+        actions   = os.listdir(base_path)
+        
+        for action in actions:
+            for video in os.listdir(os.path.join(base_path, action)):
+                if not '.DS' in video:
+                    video_images = sorted(os.listdir(os.path.join(base_path, action, video.replace('.avi',''))))
+                    self.samples.append([os.path.join(base_path, action, video.replace('.avi',''), video_image) for video_image in video_images])
+                    self.labels.append(label_dict[action])
+            
+                # END IF
+
+            # END FOR
+
+        # END FOR
+
         # TODO Implement JSON reader
         return []
 
