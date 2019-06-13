2cf0a9178a4d512a378481ea5170a584575d8e51
Eric Hofesmann <erichof@umich.edu>
2019-06-13 18:12:52 -0400

    Added preprocFrame and preprocClip to VideoDataset, added JSON parser for DetectionDataset, added __getitem__ for ImageNetVID Dataset

--- datasets/ImageNetVID.py
+++ datasets/ImageNetVID.py
@@ -3,164 +3,86 @@ from abstract_datasets import DetectionDataset
 from PIL import Image
 import os
 import numpy as np
-#import cv2
-
 
 class ImageNetVID(DetectionDataset):
     def __init__(self, *args, **kwargs):
         super(ImageNetVID, self).__init__(*args, **kwargs)
 
+        #self.labels_dict = np.load('/z/home/erichof/datasets/ILSVRC2015/labels_number_keys.npy').tolist()
+        #self.label_values = self.labels_dict.values()
+        #self.label_values.sort()
+
+
+        # Maximum number of annotated object present in a single frame in entire dataset
+        # Dictates the return size of annotations in __getitem__
+        self.max_objects = 22
+
     def __getitem__(self, idx):
-        # TODO Update placeholder getitem
-
-        # Load the frame images individually along with the annotation xml data, then combine to form the video
-        images  = []
-        annots  = []
-
-        vidname = self.samples[idx]
-        base_data_path = os.path.join(self.dataset_dir,'Data','VID',self.train_or_val, vidname)
-        base_anno_path = os.path.join(self.dataset_dir,'Annotations','VID',self.train_or_val, vidname)
-
-        frames = os.listdir(base_data_path)
-        frames.sort()
-
-        # Initialize the bbox coordinates and labels to the maximum number possible in the dataset (max is 22 objects in a single frame for imagenet vid)
-        # Init the coordinates to -1
-        xmin    = np.zeros((len(frames),self.maxbboxes))-1 
-        xmax    = np.zeros((len(frames),self.maxbboxes))-1 
-        ymin    = np.zeros((len(frames),self.maxbboxes))-1 
-        ymax    = np.zeros((len(frames),self.maxbboxes))-1 
-        label   = np.zeros((len(frames),self.maxbboxes))-1 
-
-        frame_cnt = 0
-
-        # Load each frame image and annotation
-        for frame_id in frames:
-            img = Image.open(os.path.join(base_data_path, frame_id))
-            img_shape = img.shape
-            img = Image.resize(img, (self.resize_shape[0], self.resize_shape[1]))
-
-            # Image values range from 0 to 1
-            img = img/255.
-            images.append(img)
-
-            annot_id = frame_id.split('.')[0]+'.xml'
-            annot_f = open(os.path.join(base_anno_path, annot_id), 'r')
-            annot = xmltodict.parse(annot_f)
-            annot_f.close()
-            annots.append(annot)
-
-            # Check if there exist any bounding boxes in the frame
-            if 'object' in annot['annotation'].keys():
-
-                # Check if there exist more than one bounding box
-                if type(annot['annotation']['object']) == list:
-                    for bbox_cnt in range(len(annot['annotation']['object'])):
-                        curr_xmax  = int(annot['annotation']['object'][bbox_cnt]['bndbox']['xmax'])
-                        curr_xmin  = int(annot['annotation']['object'][bbox_cnt]['bndbox']['xmin'])
-                        curr_ymax  = int(annot['annotation']['object'][bbox_cnt]['bndbox']['ymax'])
-                        curr_ymin  = int(annot['annotation']['object'][bbox_cnt]['bndbox']['ymin'])
-
-                        # Get the dataset label for the bbox object
-                        temp_label = annot['annotation']['object'][bbox_cnt]['name']
-
-                        # Get the human readable label for the bbox object (TODO: currently not used, figure out how to return string)
-                        curr_label_name = self.labels_dict[temp_label] 
-
-                        # Get the integer label for the bbox object
-                        curr_label = int(self.label_values.index(curr_label_name))
-
-                        # Resize bbox to match the resized image
-                        curr_xmin, curr_xmax, curr_ymin, curr_ymax = self.resize_bbox(curr_xmin, curr_xmax, curr_ymin, curr_ymax, img_shape, self.resize_shape)
-                                                                                                                                                                
-                        
-                        xmax[frame_cnt, bbox_cnt]  = curr_xmax
-                        xmin[frame_cnt, bbox_cnt]  = curr_xmin
-                        ymax[frame_cnt, bbox_cnt]  = curr_ymax
-                        ymin[frame_cnt, bbox_cnt]  = curr_ymin
-                        label[frame_cnt, bbox_cnt] = curr_label
-
-
-
-                else:
-                    curr_xmax = int(annot['annotation']['object']['bndbox']['xmax'])
-                    curr_xmin = int(annot['annotation']['object']['bndbox']['xmin'])
-                    curr_ymax = int(annot['annotation']['object']['bndbox']['ymax'])
-                    curr_ymin = int(annot['annotation']['object']['bndbox']['ymin'])
-
-                    # Get the dataset label for the bbox object
-                    temp_label = annot['annotation']['object']['name']
-
-                    # Get the human readable label for the bbox object (TODO: currently not used, figure out how to return string)
-                    curr_label_name = self.labels_dict[temp_label] 
-
-                    # Get the integer label for the bbox object
-                    curr_label = int(self.label_values.index(curr_label_name))
-            
-                    # Resize bbox to match the resized image
-                    curr_xmin, curr_xmax, curr_ymin, curr_ymax = self.resize_bbox(curr_xmin, curr_xmax, curr_ymin, curr_ymax, img_shape, self.resize_shape)
-    
-                    
-                    xmax[frame_cnt, 0]  = curr_xmax
-                    xmin[frame_cnt, 0]  = curr_xmin
-                    ymax[frame_cnt, 0]  = curr_ymax
-                    ymin[frame_cnt, 0]  = curr_ymin
-                    label[frame_cnt, 0] = curr_label
-
-            frame_cnt += 1
-
+        vid_info = self.samples[idx]
         
-        # Extend the video and annotations if the length is lower than the required seq_length
-        if len(images) < self.seq_length:
-            images = np.repeat(images, math.ceil(self.seq_length/float(len(images))), axis=0).tolist()
-            annots = np.repeat(annots, math.ceil(self.seq_length/float(len(annots))), axis=0).tolist()
-            label  = np.repeat(label, math.ceil(self.seq_length/float(len(label))), axis=0).tolist()
+        # TODO Rename dictionary keys
+        base_path = vid_info['base_path']
+        #vid_size  = vid_info['frame_size']
+        vid_size  = vid_info['frames'][0]['frame_sz']
+
+        vid_data   = np.zeros((self.clip_length, self.final_shape[0], self.final_shape[1], 3))-1
+        bbox_data  = np.zeros((self.clip_length, self.max_objects, 4))-1
+        labels     = np.zeros((self.clip_length, self.max_objects))-1
+        occlusions = np.zeros((self.clip_length, self.max_objects))-1
+
+
+
+
+        for frame_ind in range(len(vid_info['frames'])):
+            frame      = vid_info['frames'][frame_ind]
+            frame_path = frame['img_path']
+            #frame_path = frame['frame_path']
             
-            xmax = np.repeat(xmax, math.ceil(self.seq_length/float(len(xmax))), axis=0).tolist()
-            xmin = np.repeat(xmin, math.ceil(self.seq_length/float(len(xmin))), axis=0).tolist()
-            ymax = np.repeat(ymax, math.ceil(self.seq_length/float(len(ymax))), axis=0).tolist()
-            ymin = np.repeat(ymin, math.ceil(self.seq_length/float(len(ymin))), axis=0).tolist()
+            # Extract bbox and label data from video info
+            for obj in frame['objs']:
+                trackid   = obj['trackid']
+                label     = obj['c']
+                occlusion = obj['occ']
+                obj_bbox  = obj['bbox'] # [xmin, ymin, xmax, ymax]
 
-        # Take the first seq_length frames and corresponding annotations
-        images = images[:self.seq_length]
-        annots = annots[:self.seq_length]
-        label  = label[:self.seq_length]
+                # TODO Generate label maps from ImageNet VID encoded label to 0-29 digit
+                label = 0 #self.label_values[label]
 
-        xmax = np.clip(xmax[:self.seq_length], a_min=-1, a_max=self.resize_shape[0]-1)
-        xmin = np.clip(xmin[:self.seq_length], a_min=-1, a_max=self.resize_shape[0]-1)
-        ymax = np.clip(ymax[:self.seq_length], a_min=-1, a_max=self.resize_shape[0]-1)
-        ymin = np.clip(ymin[:self.seq_length], a_min=-1, a_max=self.resize_shape[0]-1)
+                bbox_data[frame_ind, trackid, :] = obj_bbox
+                labels[frame_ind, trackid]       = label 
+                occlusions[frame_ind, trackid]   = occlusion
 
-        
-        # Convert frames to pytorch tensor
-        images = np.stack(images)
-        images = torch.from_numpy(images)
+            # Load frame image data, preprocess image, and augment bounding boxes accordingly
+            # TODO: Augment bounding boxes according to frame augmentations 
+            vid_data[frame_ind], bbox_data[frame_ind] = self._preprocFrame(os.path.join(base_path, frame_path), bbox_data[frame_ind])
 
 
-        # Permute the opencv dimensions (Frame, Height, Width, Chan) to pytorch (Chan, frame, height, width) 
-        images = images.permute(3, 0, 1, 2)
+        bbox_data = bbox_data.astype(int)
+
+        xmin_data  = torch.from_numpy(bbox_data[:,:,0])
+        ymin_data  = torch.from_numpy(bbox_data[:,:,1])
+        xmax_data  = torch.from_numpy(bbox_data[:,:,2])
+        ymax_data  = torch.from_numpy(bbox_data[:,:,3])
+        vid_data   = torch.from_numpy(vid_data)
+        labels     = torch.from_numpy(labels)
+        occlusions = torch.from_numpy(occlusions)
+
+        # Permute the PIL dimensions (Frame, Height, Width, Chan) to pytorch (Chan, frame, height, width) 
+        vid_data = vid_data.permute(3, 0, 1, 2)
 
 
-        xmax  = torch.from_numpy(np.array(xmax).astype(int))
-        xmin  = torch.from_numpy(np.array(xmin).astype(int))
-        ymax  = torch.from_numpy(np.array(ymax).astype(int))
-        ymin  = torch.from_numpy(np.array(ymin).astype(int))
-        label = torch.from_numpy(np.array(label).astype(int))
-
-
-        ret_dict = {}
-        ret_dict['data'] = images
-        ret_dict['true_labels'] = label
-        ret_dict['label_names'] = self.label_values
-        #ret_dict['annots'] = annots
-        ret_dict['xmax'] = xmax
-        ret_dict['xmin'] = xmin
-        ret_dict['ymax'] = ymax
-        ret_dict['ymin'] = ymin
+        ret_dict = dict() 
+        ret_dict['data']       = vid_data 
+        ret_dict['xmin']       = xmin_data
+        ret_dict['ymin']       = ymin_data
+        ret_dict['xmax']       = xmax_data
+        ret_dict['ymax']       = ymax_data
+        ret_dict['labels']     = labels
+        ret_dict['occlusions'] = occlusions
 
         return ret_dict
 
-
+    # TODO Move to VideoDataset._preprocFrame
     def resize_bbox(self, xmin, xmax, ymin, ymax, img_shape, resize_shape):
         # Resize a bounding box within a frame relative to the amount that the frame was resized
 
@@ -182,5 +104,5 @@ class ImageNetVID(DetectionDataset):
         return xmin_new, xmax_new, ymin_new, ymax_new 
 
 
-dataset = ImageNetVID(16, 128, 128)
+dataset = ImageNetVID(json_path='/y/datasets/ILSVRC2015', dataset_type='train')
 import pdb; pdb.set_trace()
--- datasets/abstract_datasets.py
+++ datasets/abstract_datasets.py
@@ -3,31 +3,83 @@ from torch.utils.data import Dataset
 import json
 import numpy as np
 import os
+from PIL import Image
 
 class VideoDataset(Dataset):
     __metaclass__ = ABCMeta
-    def __init__(self, seq_length, resize_shape, crop_shape, crop_type='random', strides=1):
+    def __init__(self, json_path, dataset_type, clip_length=16, clip_offset=0, clip_stride=1, num_clips=-1, resize_shape=[128, 128], crop_shape=[128, 128], crop_type='random', final_shape=[128,128]):
         """
         Args: 
-            seq_length:   Number of frames in each clip that will be input into the network
+            json_path:    Path to the directory containing the dataset's JSON file (not including the file itself)
+            dataset_type: String indicating whether to load training or validation data ('train' or 'val') 
+            clip_length:  Number of frames in each clip that will be input into the network
+            clip_offset:  Number of frames from beginning of video to start extracting clips 
+            clip_stride:  The temporal stride between clips
+            num_clips:    Number of clips to extract from each video (-1 uses the entire video)
             resize_shape: The shape [h, w] of each frame after resizing
             crop_shape:   The shape [h, w] of each frame after cropping
             crop_type:    The method used to crop (either random or center)
-            strides:      The temporal stride between clips
+            final_shape:  Final shape [h, w] of each frame after all preprocessing, this is input to network
         """
-        self.seq_length     = seq_length 
+        # JSON loading arguments
+        self.json_path      = json_path 
+        self.dataset_type   = dataset_type
+        
+        # Clips processing arguments
+        self.clip_length    = clip_length 
+        self.clip_offset    = clip_offset
+        self.clip_stride    = clip_stride
+        self.num_clips      = num_clips
+
+        # Frame-wise processing arguments
         self.resize_shape   = resize_shape 
         self.crop_shape     = crop_shape 
         self.crop_type      = crop_type 
-        self.strides        = strides 
+        self.final_shape    = final_shape
 
-        self.getClips()
+        # Creates the self.samples list which will be indexed by each __getitem__ call
+        self._getClips()
+
 
     def __getitem__(self, idx):
         raise NotImplementedError("Dataset must contain __getitem__ method which loads videos from memory.")
 
-    def getClips(self):
+    def _getClips(self):
+        """
+        Loads the JSON file associated with the videos in a datasets and processes each of them into clips
+        """
         raise NotImplementedError("Dataset must contain getClips method which loads and processes the dataset's JSON file.") 
+
+    def _processClips(self, video):
+        """
+        Processes a single video into uniform sized clips that will be loaded by __getitem__
+        Args:
+            video:       List containing a dictionary of annontations per frame
+
+        Additional Parameters:
+            self.clip_length: Number of frames extracted from each clip
+            self.num_clips:   Number of clips to extract from each video (-1 uses the entire video)
+            self.clip_offset: Number of frames from beginning of video to start extracting clips 
+            self.clip_stride: Number of frames between clips when extracting them from videos 
+        """
+        return [video[:self.clip_length]]
+
+    def _preprocFrame(self, frame_path, bbox_data=[]):
+        """
+        Preprocess a frame using data augmentation functions and return processed image 
+        Args:
+            frame_path: The global path to the frame image 
+            bbox_data:  For detection datasets, augment bbox coordinates according to frame augmentations (size = [# objects, 4 coordinates])
+        """
+        frame_data = Image.open(frame_path)
+        frame_data = frame_data.resize(self.final_shape)
+        # TODO @preetsg add calls to preproc_utils.py here
+
+        if bbox_data==[]:
+            return np.array(frame_data)
+
+        else:
+            return np.array(frame_data), bbox_data
         
 
 
@@ -36,7 +88,28 @@ class RecognitionDataset(VideoDataset):
     def __init__(self, *args, **kwargs):
         super(RecognitionDataset, self).__init__(*args, **kwargs)
 
-    def getClips(self):
+    def _getClips(self):
+        """
+        Required format for all recognition dataset JSON files:
+        
+        List(Vidnumber: Dict{
+                   List(Frames: Dict{
+                                     Frame Size,
+                                     Frame Path,
+                                     List(Actions: Dict{
+                                                        Track ID
+                                                        Action Class
+                                     }) End Object List in Frame
+        
+                   }) End Frame List in Video
+        
+                   Str(Base Vid Path)
+        
+             }) End Video List in Dataset
+        
+        Eg: action_label = dataset[vid_index]['frames'][frame_index]['actoins'][action_index]['action_class']
+        """
+
         data_path  = os.path.join('/z/dat', self.dataset_dir)
         label_dict = {'brush_hair': 0,      'cartwheel': 1,
                       'catch': 2,           'chew': 3, 
@@ -66,7 +139,7 @@ class RecognitionDataset(VideoDataset):
                       'wave': 50}
         
    
-        base_path = os.path.join(data_path, self.train_or_val+'images')
+        base_path = os.path.join(data_path, self.dataset_type+'images')
         actions   = os.listdir(base_path)
         
         for action in actions:
@@ -82,9 +155,6 @@ class RecognitionDataset(VideoDataset):
 
         # END FOR
 
-        # TODO Implement JSON reader
-        return []
-
 
 
 
@@ -93,56 +163,53 @@ class DetectionDataset(VideoDataset):
     def __init__(self, *args, **kwargs):
         super(DetectionDataset, self).__init__(*args, **kwargs)
 
-    def getClips(self):
-        #json_train_path = '/z/home/natlouis/pytorch_goturn/data/ILSVRC2015_VID/ilsvrc_train.json'
-        #json_train_file = open(json_train_path,'r')
-        #json_train_data = json.load(json_train_file) 
-        #json_train_file.close()
-
-        #json_val_path = '/z/home/natlouis/pytorch_goturn/data/ILSVRC2015_VID/ilsvrc_val.json'
-        #json_val_file = open(json_val_path,'r')
-        #json_val_data = json.load(json_val_file) 
-        #json_val_file.close()
-        #
-        #json_snippet_train_path = '/z/home/natlouis/pytorch_goturn/data/ILSVRC2015_VID/ilsvrc_train_snippet.json'
-        #json_snippet_train_file = open(json_snippet_train_path,'r')
-        #json_snippet_train_data = json.load(json_snippet_train_file) 
-        #json_snippet_train_file.close()
-
-        #json_snippet_val_path = '/z/home/natlouis/pytorch_goturn/data/ILSVRC2015_VID/ilsvrc_val_snippet.json'
-        #json_snippet_val_file = open(json_snippet_val_path,'r')
-        #json_snippet_val_data = json.load(json_snippet_val_file) 
-        #json_snippet_val_file.close()
-
-        #self.train_samples = []
-        #self.train_labels  = []
-        #for vid in json_snippet_train_data:
-        #    self.train_samples.append(
-
-        #self.labels_dict = np.load('/z/home/erichof/datasets/ILSVRC2015/labels_number_keys.npy').tolist()
-        #self.label_values = self.labels_dict.values()
-        #self.label_values.sort()
-
-
+    def _getClips(self):
+        """
+        Required format for all detection datset JSON files:
+        
+        List(Vidnumber: Dict{
+                   List(Frames: Dict{
+                                     Frame Size,
+                                     Frame Path,
+                                     List(Objects: Dict{
+                                                        Track ID
+                                                        Object Class
+                                                        Occluded
+                                                        List(Bounding box coordinates: [xmin, ymin, xmax, ymax])
+        
+                                     }) End Object List in Frame
+        
+                   }) End Frame List in Video
+        
+                   Str(Base Vid Path)
+        
+             }) End Video List in Dataset
+        
+        Ex: coordinates = dataset[vid_index]['frames'][frame_index]['objects'][obj_index]['bbox']
+        """
 
         # Load all video paths into the samples array to be loaded by __getitem__ 
 
         self.samples = []
-        self.dataset_dir = '/z/home/natlouis/data/ILSVRC2015/'
-        self.train_or_val = 'train'
+        self.dataset_type = 'train'
         
-        annot_path = os.path.join(self.dataset_dir, 'Annotations/VID/')
-        #data_path = os.path.join(self.dataset_dir, 'Data/VID/')
-
-        
-        if self.train_or_val == 'val':
-            self.samples = os.listdir(os.path.join(annot_path, self.train_or_val))
-            self.samples.sort()
+        if self.dataset_type == 'train':
+            full_json_path = os.path.join(self.json_path, 'train.json')
 
         else:
-            self.samples = []
-            for vid_dir in os.listdir(os.path.join(annot_path, self.train_or_val)):
-                self.samples = self.samples + [ vid_dir + '/' + s for s in os.listdir(os.path.join(annot_path, self.train_or_val, vid_dir))]
+            full_json_path = os.path.join(self.json_path, 'val.json') 
 
+        json_file = open(full_json_path,'r')
+        json_data = json.load(json_file) 
+        json_file.close()
+
+        # Load the information for each video and process it into clips
+        for video_info in json_data:
+            clips = self._processClips(video_info['frame'])
+
+            # Each clip is a list of dictionaries per frame containing information
+            # Example info: object bbox annotations, object classes, frame img path
+            for clip in clips:    
+                self.samples.append(dict(frames=clip, base_path=video_info['base_path']))
 
 
