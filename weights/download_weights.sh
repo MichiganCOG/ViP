1259878b3b36a8a2d7f3eaecaa23254ae153d599
Nathan Louis <natlouis@umich.edu>
2019-06-14 15:41:23 -0400

    Add script to download model weights

---
+++ weights/download_weights.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#wget -O [saved_file_name] [direct_download_link]
+
+#GoTurn
+wget -O ./weights/goturn.pth.tar https://umich.box.com/shared/static/hdbh9vxdoapwpau4vj5oftna91dqrbo3.tar
