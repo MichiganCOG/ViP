9a9445bbd781f95c300f70a58b2bd13861fcb8cf
Nathan Louis <natlouis@umich.edu>
2019-06-14 16:11:51 -0400

    changes to model import and download weights

--- models/models_import.py
+++ models/models_import.py
@@ -1,4 +1,3 @@
-import glob
 import importlib
 import sys
 
@@ -15,12 +14,10 @@ def create_model_object(**kwargs):
 
     model_name = kwargs['model_name']
     del kwargs['model_name']
-    model_file = glob.glob('models/'+model_name+'.py')
+    model_file = 'models/'+model_name+'.py'
 
     if not model_file:
         sys.exit('Model not found. Ensure model is in models/, with a matching name')
-    if len(model_file) > 1:
-        sys.exit('Multiple matching files found: {}'.format(model_file)) #shouldn't occur
 
     module_name = model_file[0][:-3].replace('/','.')
     module = importlib.import_module(module_name)
--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -3,4 +3,4 @@
 #wget -O [saved_file_name] [direct_download_link]
 
 #GoTurn
-wget -O ./weights/goturn.pth.tar https://umich.box.com/shared/static/hdbh9vxdoapwpau4vj5oftna91dqrbo3.tar
+wget -O ./weights/goturn.pth.tar https://umich.box.com/shared/static/src6rfm4lpn0v3t4l26d6u0v4ixdwem5.tar
