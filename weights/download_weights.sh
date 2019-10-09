b34b0eec63489428907bf2681212cc5187145434
Nathan Louis <natlouis@umich.edu>
2019-10-09 16:23:22 -0400

    Added download links for weight and data files

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -2,9 +2,6 @@
 
 #wget -O [saved_file_name] [direct_download_link]
 
-#GoTurn
-wget -O ./weights/goturn.pth.tar https://umich.box.com/shared/static/src6rfm4lpn0v3t4l26d6u0v4ixdwem5.tar
-
 #SSD
 wget -O ./weights/ssd300_mAP_77.43_v2.pkl https://umich.box.com/shared/static/jszcnnwcvscfyqe3o81xy8qzfbsc20vo.pkl
 
@@ -13,3 +10,6 @@ wget -O ./weights/c3d-pretrained.pth https://umich.box.com/shared/static/znmyt8u
 
 #C3D Mean
 wget -O ./weights/sport1m_train16_128_mean.npy https://umich.box.com/shared/static/ppbnldsa5rty615osdjh2yi8fqcx0a3b.npy 
+
+#YC2BB-Full model
+wget -O ./weights/yc2bb_full-model.pth https://umich.box.com/shared/static/5ukbdcawryzkkq4r789z0src6u6uvg3u.pth 
